adf291ec2bad40206967bbfd5aebd11d6b94209b
Utkarsh Singh <utkarsh2.singh.com>
2019-08-25 23:46:43 +0530

    assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import AboutUs from "./AboutComponent";
 
 class Main extends Component {
 
@@ -49,6 +50,8 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
+              <Route path="/aboutus" component={() => <AboutUs leaders={this.state.leaders} />}
+            />
               <Redirect to="/home" />
           </Switch>
         <Footer />
