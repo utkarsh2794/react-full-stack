27b00255fb44b1b37f7d7a5eccc04eb6ade1ec3f
Utkarsh Singh <utkarsh2.singh.com>
2019-09-25 01:57:43 +0530

    redux part 2

--- confusion/components/AboutComponent.js
+++ confusion/components/AboutComponent.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
 import { ScrollView, View, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { LEADERS } from '../shared/leaders';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
-const mapStateToProps = state => {
-    return {
-      leaders: state.leaders
-    }
+const mapStateToProps = (state) => {
+  return {
+    leaders: state.leaders
   }
+}
 
 const Leaders = (props) => {
-
+  
   const renderLeaders = ({item, index}) => {
     return(
       <ListItem
@@ -20,18 +19,18 @@ const Leaders = (props) => {
         title={item.name}
         subtitle={item.description}
         hideChevron={true}
-        leftAvatar={{source: {uri: baseUrl + item.image}}}
+        leftAvatar={{source: {uri: baseUrl + item.image }}}
       />
     )
   };
 
   return(
     <Card title='Corporate Leadership'>
-      <FlatList 
-        data={this.props.leaders.leaders}
+      <FlatList
+        data={props.leaders}
         renderItem={renderLeaders}
         keyExtractor={item => item.id.toString()}
-        />
+      />
     </Card>
   );
 };
@@ -54,14 +53,37 @@ class About extends Component {
   static navigationOptions = {
     title: 'About Us'
   }
-
+  
   render(){
-    return(
-      <ScrollView>
-        <History/>
-        <Leaders leaders={LEADERS}/>
-      </ScrollView>
-    );
+    if (this.props.leaders.isLoading) {
+      return(
+          <ScrollView>
+              <History />
+              <Card
+                title='Corporate Leadership'>
+              </Card>
+          </ScrollView>
+      );
+    }
+    else if (this.props.leaders.errMess) {
+      return(
+          <ScrollView>
+              <History />
+              <Card
+                title='Corporate Leadership'>
+                <Text>{this.props.leaders.errMess}</Text>
+              </Card>
+          </ScrollView>
+      );
+    }
+    else {
+      return(
+        <ScrollView>
+          <History/>
+          <Leaders leaders={this.props.leaders.leaders}/>
+        </ScrollView>
+      );
+    }
   }
 }
 
